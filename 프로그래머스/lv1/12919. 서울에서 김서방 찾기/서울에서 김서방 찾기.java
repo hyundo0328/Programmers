7fa79e03ee946a41d772c229e576ba8026d14dc6
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-07-30 02:10:07 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.59 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int x = 0;
+
+        for(String value:seoul) {
+          if (value.equals("Kim")) break;
+          x++;
+        }
+
+        return "김서방은 "+ x + "에 있다";
+    }
+}
