95844c90d023a544dabb8e650a824dd7b23900b7
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-08-05 15:46:58 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.py
+++ 프로그래머스/lv1/70128. 내적/내적.py
@@ -1,7 +1,8 @@
 def solution(a, b):
-    answer = 0
+#     answer = 0
 
-    for i in range(0,len(a)):
-        answer += a[i]*b[i]
+#     for i in range(0,len(a)):
+#         answer += a[i]*b[i]
         
-    return answer
+    # zip을 이용한 배열 불러오기
+    return sum([x*y for x, y in zip(a,b)])
