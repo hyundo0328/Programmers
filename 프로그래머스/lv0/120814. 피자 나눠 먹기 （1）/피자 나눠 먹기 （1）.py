609d3347a6191dba8277ff74e3bfca15024a7ecf
박현도 <117895948+hyundo0328@users.noreply.github.com>
2023-08-10 23:53:40 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,7 +1,10 @@
+import math
+
 def solution(n):
     # if(n%7 == 0):
     #     return n//7
     # else:
     #     return n//7+1
     
-    return (n - 1) // 7 + 1
+    # return (n - 1) // 7 + 1
+    return math.ceil(n/7)
